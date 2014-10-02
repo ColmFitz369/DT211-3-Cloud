345bda9375bb9d778b45b0d55d9800e6256f2392
ColmFitz369 <colm_fitzpatrick369@hotmail.com>
2014-10-02 14:18:19 +0000

    lowestdi

---
+++ lowestdiv.py
@@ -0,0 +1,17 @@
+number = 1
+div = 1
+answer = 0
+
+while div <=10 and answer != 10:
+
+    if number % div == 0:
+    
+        answer += 1
+        div += 1
+    
+    else:
+
+        div = 1
+
+print (number)
+
